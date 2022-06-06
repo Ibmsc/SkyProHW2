20f5191947dd21c1ea2acd4a190357ae1c2672d2
ibmsc <Ibm.sc1409@gmail.com>
2022-06-06 22:05:43 +0300

    Второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
-    //Задача №1
-
-      byte b = 125;
-      short s = 234;
-      int i = 1;
-      Long l = 922L;
-      float f = 77.77F;
-      double d = 16.16;
-      boolean booIsAdult = b > 126; // true или false;
-      char simbol = 33;
 
 
-    // Задача 2
+        //Задача №1
 
-    double boxer1 = 78.2;
-    double boxer2 = 82.7;
-    double SummaWeightAllBoxer = boxer1 + boxer2;
-    double DifferenceWeithAllBoxer = boxer2 - boxer1;
+        byte b = 125;
+        short s = 234;
+        int i = 1;
+        Long l = 922L;
+        float f = 77.77F;
+        double d = 16.16;
+        boolean booIsAdult = b > 126; // true или false;
+        char simbol = 33;
+
+
+        // Задача 2
+
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double SummaWeightAllBoxer = boxer1 + boxer2;
+        double DifferenceWeithAllBoxer = boxer2 - boxer1;
         System.out.println("Разница в весе " + DifferenceWeithAllBoxer + " Кг.");
         System.out.println("Общий вес боксеров " + SummaWeightAllBoxer + " Кг.");
 
 
+        // Задача 3
+
+
+    }
+
+    ;
 }
 
     // System.out.println("Пишем дз");
