76d95525c519cc1b6d420316d1b12a07c1563bfe
ibmsc <Ibm.sc1409@gmail.com>
2022-06-07 08:29:24 +0300

    6 коммит

--- src/Main.java
+++ src/Main.java
@@ -32,8 +32,8 @@ public class Main {
         int eggs = 70 * 4;
         int mixBlander = bananas + milk + icecream + eggs;
         float mixBlanderKg = mixBlander / 1000f;
-
         System.out.println("Всего вес спорт-завтрака " + mixBlander + " граммов " + " или " + mixBlanderKg + " Кг");
+
 
         // Задача 4
         int needLeft = 7*1000;
@@ -43,6 +43,28 @@ public class Main {
         System.out.println( leftInDay1 +" дней при потере по 250г. в день" + " или " +
                 leftInDay2 + " дней при потере по 500г. в день"  );
         System.out.println("В среднем может потребоваться " + midDay + " дней.");
+
+
+        // Задача 5
+
+        int salaryMasha = 67_760;
+        int salaryDenis = 83_690;
+        int salaryKristina = 76_230;
+
+        int salaryMasha10 = (salaryMasha * 10) / 100 + salaryMasha;
+        int salaryDenis10 = (salaryDenis*10) / 100 + salaryDenis;
+        int salaryKristina10 = (salaryKristina*10) /100 + salaryKristina;
+
+        int salaryMashaYear = (salaryMasha10 * 12) - (salaryMasha * 12) ;
+        int salaryDenisYear = (salaryDenis10*12) - (salaryDenis * 12);
+        int salaryKristinaYear = (salaryKristina10*12) - (salaryKristina * 12);
+
+        System.out.println("Маша теперь получает " + salaryMasha10 + " рублей. " +
+                "Годовой доход вырос на " + salaryMashaYear + " рублей. ");
+        System.out.println("Денис теперь полчает " + salaryDenis10 + " рублей." +
+                "Годовой доход вырос на " + salaryDenisYear + " рублей." );
+        System.out.println("Кристина теперь получает " + salaryKristina10 + " рублей. " +
+                "Годовой доход вырос на " + salaryKristinaYear + " рублей. ");
     }
 }
 // System.out.println("Пишем дз");
